8d9a62d1baefa851d20cf45b52597c2494922dda
kimi5231 <djwjod@naver.com>
2024-09-10 23:40:23 +0900

    변수 기초 완료

---
+++ GPIA1/GPIA1/GPIA1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+// [타입] [이름];
+
+// char : 1
+// short : 2
+// int : 4
+// longlong(__int64) : 8
+
+int hp;
+int maxHp;
+
+int main()
+{
+    maxHp = 200;
+
+    hp = maxHp;
+
+    cout << hp;
+}
