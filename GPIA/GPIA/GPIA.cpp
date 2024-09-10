35139902d9dd5365c4342bf40905523e6192dc17
kimi5231 <djwjod@naver.com>
2024-09-10 17:20:21 +0900

    기본 템플릿 분석 완료

---
+++ GPIA/GPIA/GPIA.cpp
@@ -0,0 +1,173 @@
+﻿// GPIA.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "GPIA.h"
+
+#define MAX_LOADSTRING 100
+
+int mousePosX;
+int mousePosY;
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    // 1) 윈도우 창 정보 등록
+    MyRegisterClass(hInstance);
+
+    // 2) 윈도우 창 생성
+    if (!InitInstance (hInstance, nCmdShow))
+        return FALSE;
+
+    MSG msg;
+
+    // 3) 메인 루프
+    // - 입력
+    // - 로직
+    // - 렌더링
+
+    while (::GetMessage(&msg, nullptr, 0, 0))
+    {
+        ::TranslateMessage(&msg);
+        ::DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GPIA));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName = nullptr;
+    // 윈도우 생성에 필요한 키 값
+    wcex.lpszClassName  = L"KEY";
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   RECT windowRect = { 0, 0, 800, 600 };
+   ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, false);
+
+   HWND hWnd = CreateWindowW(L"KEY", L"GPIA", WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, windowRect.right - windowRect.left, windowRect.bottom - windowRect.top, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ::ShowWindow(hWnd, nCmdShow);
+   ::UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_EXIT:
+                ::DestroyWindow(hWnd);
+                break;
+            default:
+                return ::DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+            // 문자
+            WCHAR buffer[100];
+            ::wsprintf(buffer, L"(%d, %d)", mousePosX, mousePosY);
+            ::TextOut(hdc, 100, 100, buffer, ::wcslen(buffer));
+
+            // 사각형
+            ::Rectangle(hdc, 200, 200, 400, 400);
+
+            // 원
+            ::Ellipse(hdc, 200, 200, 400, 400);
+
+            // 선
+            ::MoveToEx(hdc, 300, 300, nullptr);
+            ::LineTo(hdc, 400, 400);
+            ::LineTo(hdc, 500, 300);
+
+            ::EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_MOUSEMOVE:
+        mousePosX = LOWORD(lParam);
+        mousePosY = HIWORD(lParam);
+        ::InvalidateRect(hWnd, nullptr, true);
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        break;
+    default:
+        return ::DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
