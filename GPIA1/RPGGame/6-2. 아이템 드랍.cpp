ed8c7dee62989dee16631c796de146e41c84d69f
kimi5231 <djwjod@naver.com>
2024-09-22 23:08:21 +0900

    아이템 드랍 완료

---
+++ GPIA1/RPGGame/6-2. 아이템 드랍.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "6-2. Item.h"
+using namespace std;
+
+Item* DropItem()
+{
+    if (rand() % 2 == 0)
+    {
+        Weapon* weapon = new Weapon;
+        return weapon;
+    }
+    else
+    {
+        Armor* armor = new Armor;
+        return armor;
+    }
+}
+
+int main()
+{
+    srand((unsigned)time(0));
+
+    for (int i = 0;i < 100; ++i)
+    {
+        Item* item = DropItem();
+
+        item->PrintInfo();
+
+        // delete item
+    }
+}
